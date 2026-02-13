8b6f73c253e2c7d157db78f9bfdbc61d982a1946
yhnujm632 <167638565+yhnujm632@users.noreply.github.com>
2026-02-13 18:18:50 -0500

    Updated dimensions after robot redesign

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 
 class Constants:
-    WIDTH = 43 # in cm
-    HEIGHT = 35 # in cm
+    WIDTH = 39.19 # in cm
+    HEIGHT = 35.5 # in cm
     STARTING_POSITION = (-143, 5)# (-143, 0) # (0,0) is the center of the mobile goal
     STARTING_ROTATION = 0.610865 # Yaw, aka around the Z/vertical axis. IDC about the other rotation angles. They're negligible IMO.
     DISTANCE_BETWEEN_WHEELS = 30.2 # in cm
